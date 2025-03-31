e1dd92a4ef58bdd0e832054a1198d97d10943774
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:15:57 +0700

    add feedBack repository

---
+++ BE/src/main/java/com/swp/project/repository/FeedbackRepository.java
@@ -0,0 +1,11 @@
+package com.swp.project.repository;
+
+import com.swp.project.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
+
+    List<Feedback> findByDoctorId(int doctorId);
+}
