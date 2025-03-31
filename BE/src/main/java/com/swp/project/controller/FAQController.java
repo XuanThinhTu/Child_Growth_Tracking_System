d85c370d7b131b0262e765de36fa817c1f993944
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:11:56 +0700

    create FAQ controller

---
+++ BE/src/main/java/com/swp/project/controller/FAQController.java
@@ -0,0 +1,12 @@
+package com.swp.project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RequestMapping("/faq")
+@RestController
+@RequiredArgsConstructor
+public class FAQController {
+
+}
