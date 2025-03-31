157e6464b305fb353dae69d94eae083163998377
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:28:31 +0700

    post mapping

--- BE/src/main/java/com/swp/project/controller/FeedbackController.java
+++ BE/src/main/java/com/swp/project/controller/FeedbackController.java
@@ -1,6 +1,13 @@
 package com.swp.project.controller;
 
+import com.swp.project.dto.request.FeedbackRequest;
+import com.swp.project.dto.response.ApiResponse;
+import com.swp.project.dto.response.FeedbackDTO;
+import com.swp.project.service.IFeedbackService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,5 +15,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/feedback")
 public class FeedbackController {
 
+    private final IFeedbackService feedbackService;
+
+    @Operation(summary = "Create feedback, feedbackType is CONSULTATION or APPOINTMENT, rating 0-5")
+
+    @PostMapping("/send")
+
+    @SecurityRequirement(name = "bearerAuth")
+
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public ApiResponse<FeedbackDTO> createFeedback(@RequestBody FeedbackRequest request) {
+        return ApiResponse.<FeedbackDTO>builder()
+
+                .message("Feedback created")
+                .data(feedbackService.createFeedback(request))
+                .build();
+
+    }
 
 }
