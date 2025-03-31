fdacce2b30a8420a286114aef161b50eb7783a19
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:09:59 +0700

    update blog func

--- BE/src/main/java/com/swp/project/controller/BlogController.java
+++ BE/src/main/java/com/swp/project/controller/BlogController.java
@@ -51,21 +51,27 @@ public class BlogController {
     }
 
     @DeleteMapping("/delete/{blogId}")
-
     @SecurityRequirement(name = "bearerAuth")
-
     @PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_ADMIN')")
-
     public ApiResponse<Void> deleteBlog(@PathVariable int blogId) throws IOException {
-
         blogService.deleteBlog(blogId);
-
         return ApiResponse.<Void>builder()
-
                 .message("Blog deleted")
-
                 .build();
-
     }
 
+
+    @PutMapping("/update/{blogId}")
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasRole('ROLE_DOCTOR')")
+    public ApiResponse<BlogDTO> updateBlog(@PathVariable int blogId,
+                                           @RequestParam("title") String title,
+                                           @RequestParam("content") String content,
+                                           @RequestParam("categoryId") int categoryId,
+                                           @RequestParam(name = "images", required = false) MultipartFile[] images) throws IOException {
+        return ApiResponse.<BlogDTO>builder()
+                .message("Blog updated")
+                .data(blogService.updateBlog(blogId, title, content, categoryId, images))
+                .build();
+    }
 }
