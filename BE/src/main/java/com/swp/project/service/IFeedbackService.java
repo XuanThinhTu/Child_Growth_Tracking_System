5ff153326e66e2dd1763372dba04ba793da247ce
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:18:11 +0700

    create service files

---
+++ BE/src/main/java/com/swp/project/service/IFeedbackService.java
@@ -0,0 +1,19 @@
+package com.swp.project.service;
+
+
+
+public interface IFeedbackService {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ BE/src/main/java/com/swp/project/service/Impl/FeedbackService.java
@@ -0,0 +1,24 @@
+package com.swp.project.service.Impl;
+
+
+import com.swp.project.service.IFeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService implements IFeedbackService {
+
+
+
+
+
+
+
+
+
+
+
+
+}
