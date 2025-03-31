17bea30bdc8e50e866bae8d258e81e1e71f65b63
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:07:44 +0700

    impl update FAQ func

--- BE/src/main/java/com/swp/project/service/Impl/FAQService.java
+++ BE/src/main/java/com/swp/project/service/Impl/FAQService.java
@@ -34,16 +34,48 @@ public class FAQService implements IFAQService {
 
     @Override
     public FAQResponse createFAQ(FAQRequest request) {
+
         Category category = categoryRepository.findById(request.categoryId())
+
                 .orElseThrow(() -> new IllegalArgumentException("Category with id: " + request.categoryId() + " not found"));
         FAQ faq = new FAQ();
+
         faq.setQuestion(request.question());
+
         faq.setAnswer(request.answer());
+
         faq.setCategory(category);
+
         faqRepository.save(faq);
+
         return faqMapper.toDto(faq);
+
+
     }
 
+
+    @Override
+    public FAQResponse updateFAQ(int fqaId, FAQRequest request) {
+
+        Category category = categoryRepository.findById(request.categoryId())
+
+                .orElseThrow(() -> new IllegalArgumentException("Category with id: " + request.categoryId() + " not found"));
+
+        FAQ faq = faqRepository.findByIdAndIsDeletedFalse(fqaId)
+
+                .orElseThrow(() -> new IllegalArgumentException("FAQ with id: " + fqaId + " not found"));
+
+        faq.setQuestion(request.question());
+
+        faq.setAnswer(request.answer());
+
+        faq.setCategory(category);
+
+        faqRepository.save(faq);
+
+        return faqMapper.toDto(faq);
+
+    }
 
 
 
