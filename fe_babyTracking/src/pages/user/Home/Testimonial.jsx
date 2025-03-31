ca1a1195b46fe86e04d0d15101de89310dd4c23c
TuXuanThinh <thinhtxse184795@fpt.edu.vn>
2025-04-01 01:03:20 +0700

    pull

--- fe_babyTracking/src/pages/user/Home/Testimonial.jsx
+++ fe_babyTracking/src/pages/user/Home/Testimonial.jsx
@@ -12,12 +12,12 @@ const Testimonial = () => {
                 {/* Nội dung trích dẫn */}
                 <div>
                     <p className="text-4xl text-gray-800 mb-8 leading-relaxed">
-                        Et eirmod et dignissim quo, ne pro dicat adolescens. His te velit
-                        doler nemorate integre. Est zril id, ad legere persius inciderint
-                        pri, vel et nemore{" "}
-                        <span className="text-purple-600">facete virtute</span>.
+                    Children are not things to be molded, but are people to be unfolded. <span className="text-purple-600">The greatest legacy</span>.  we can give
+                    our children is not things, but an understanding of themselves
+                    and the world around them.{" "}
+                        
                     </p>
-                    <p className="text-2xl font-semibold text-gray-900">Jake Chaps</p>
+                    <p className="text-2xl font-semibold text-gray-900">Dr. Kelly Flanagan</p>
                     <p className="text-xl text-gray-500">Nutritionist</p>
                 </div>
             </div>
