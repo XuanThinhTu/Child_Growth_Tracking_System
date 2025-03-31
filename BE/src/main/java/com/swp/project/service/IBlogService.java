a1c887e92a66355eb5b8c0d0f743cdd19a6fdfb3
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:48:24 +0700

    create blog func

--- BE/src/main/java/com/swp/project/service/IBlogService.java
+++ BE/src/main/java/com/swp/project/service/IBlogService.java
@@ -1,14 +1,14 @@
 package com.swp.project.service;
 
 
+import com.swp.project.dto.response.BlogDTO;
+import org.springframework.web.multipart.MultipartFile;
 
-
-
-
+import java.io.IOException;
 
 public interface IBlogService {
 
-
+    BlogDTO createBlog(String title, String content, int categoryId, MultipartFile[] images) throws IOException;
 
 
 
--- BE/src/main/java/com/swp/project/service/Impl/BlogService.java
+++ BE/src/main/java/com/swp/project/service/Impl/BlogService.java
@@ -1,16 +1,97 @@
 package com.swp.project.service.Impl;
 
+import com.swp.project.dto.response.BlogDTO;
+import com.swp.project.entity.Blog;
+import com.swp.project.entity.BlogImage;
+import com.swp.project.entity.Category;
+import com.swp.project.entity.User;
+import com.swp.project.mapper.BlogMapper;
+import com.swp.project.repository.BlogImageRepository;
+import com.swp.project.repository.BlogRepository;
+import com.swp.project.repository.CategoryRepository;
 import com.swp.project.service.IBlogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BlogService implements IBlogService {
 
+    private final BlogRepository blogRepository;
 
+    private final BlogImageRepository blogImageRepository;
+
+    private final UserService userService;
+
+    private final CloudinaryService cloudinaryService;
+
+    private final CategoryRepository categoryRepository;
+
+
+    private final BlogMapper blogMapper;
+
+
+
+    @Override
+    public BlogDTO createBlog(String title, String content, int categoryId, MultipartFile[] images) throws IOException {
+        User user = userService.getAuthenticatedUser();
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        Blog blog = new Blog();
+        blog.setTitle(title);
+
+        blog.setContent(content);
+
+        blog.setCreatedAt(Date.valueOf(LocalDate.now()));
+
+        blog.setUpdatedAt(Date.valueOf(LocalDate.now()));
+
+        blog.setCategory(category);
+
+        List<BlogImage> blogImages = new ArrayList<>();
+
+
+        if(images != null) {
+            for (MultipartFile image : images) {
+                Map map = cloudinaryService.upload(image);
+                String url = (String) map.get("secure_url");
+                String publicId = (String) map.get("public_id");
+
+                BlogImage blogImage = new BlogImage();
+                blogImage.setUrl(url);
+
+                blogImage.setPublicId(publicId);
+
+                blogImage.setBlog(blog);
+
+                blogImages.add(blogImage);
+
+            }
+        }
+        blog.setBlogImages(blogImages);
+
+        blogRepository.save(blog);
+
+
+        for(BlogImage blogImage : blogImages){
+
+            blogImageRepository.save(blogImage);
+
+        }
+
+        return blogMapper.toBlogDTO(blog);
+
+    }
 
 
 }
