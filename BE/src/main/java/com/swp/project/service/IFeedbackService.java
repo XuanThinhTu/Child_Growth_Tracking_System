64da608c8054ad8620d3bbcf36051cc077ee2ddd
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:23:12 +0700

    createFeedback func

--- BE/src/main/java/com/swp/project/service/IFeedbackService.java
+++ BE/src/main/java/com/swp/project/service/IFeedbackService.java
@@ -1,9 +1,12 @@
 package com.swp.project.service;
 
 
+import com.swp.project.dto.request.FeedbackRequest;
+import com.swp.project.dto.response.FeedbackDTO;
 
 public interface IFeedbackService {
 
+    FeedbackDTO createFeedback(FeedbackRequest request);
 
 
 
--- BE/src/main/java/com/swp/project/service/Impl/FeedbackService.java
+++ BE/src/main/java/com/swp/project/service/Impl/FeedbackService.java
@@ -1,9 +1,18 @@
 package com.swp.project.service.Impl;
 
 
+import com.swp.project.dto.request.FeedbackRequest;
+import com.swp.project.dto.response.FeedbackDTO;
+import com.swp.project.entity.Feedback;
+import com.swp.project.entity.User;
+import com.swp.project.mapper.FeedbackMapper;
+import com.swp.project.repository.FeedbackRepository;
+import com.swp.project.repository.UserRepository;
 import com.swp.project.service.IFeedbackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 
 @Service
@@ -11,7 +20,40 @@ import org.springframework.stereotype.Service;
 public class FeedbackService implements IFeedbackService {
 
 
+    private final FeedbackRepository feedbackRepository;
+    private final FeedbackMapper feedbackMapper;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
+    @Override
+    public FeedbackDTO createFeedback(FeedbackRequest request) {
+        Feedback feedback = new Feedback();
+
+
+        feedback.setRating(request.getRating());
+
+        feedback.setCreatedAt(LocalDateTime.now());
+
+        feedback.setComment(request.getComment());
+
+        feedback.setFeedbackType(request.getFeedbackType());
+
+        feedback.setMember(userService.getAuthenticatedUser());
+
+        User doctor = userRepository.findById(request.getDoctorId())
+
+                .orElseThrow(() -> new RuntimeException("Doctor not found"));
+
+        feedback.setDoctor(doctor);
+
+
+        feedbackRepository.save(feedback);
+
+
+        return feedbackMapper.toFeedbackDTO(feedback);
+
+
+    }
 
 
 
