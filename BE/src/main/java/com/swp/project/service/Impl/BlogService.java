9dbaef935e04fb6dcd1650a1912dfefc5b5997c3
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:44:37 +0700

    add service files

---
+++ BE/src/main/java/com/swp/project/service/Impl/BlogService.java
@@ -0,0 +1,16 @@
+package com.swp.project.service.Impl;
+
+import com.swp.project.service.IBlogService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BlogService implements IBlogService {
+
+
+
+
+}
