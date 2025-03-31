f038282000aafddac02ca75d65be7b94b92916f6
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:07:06 +0700

    add get mapping

--- BE/src/main/java/com/swp/project/controller/BlogController.java
+++ BE/src/main/java/com/swp/project/controller/BlogController.java
@@ -18,31 +18,29 @@ public class BlogController {
 
     private final IBlogService blogService;
     @PostMapping("/create")
-
     @SecurityRequirement(name = "bearerAuth")
-
     @PreAuthorize("hasRole('ROLE_DOCTOR')")
-
     public ApiResponse<BlogDTO> createBlog(@RequestParam("title") String title,
-
                                            @RequestParam("content") String content,
-
                                            @RequestParam("categoryId") int categoryId,
-
                                            @RequestParam(name = "images", required = false) MultipartFile[] images) throws IOException {
+        return ApiResponse.<BlogDTO>builder()
+                .message("Blog created")
+                .data(blogService.createBlog(title, content, categoryId, images))
+                .build();
+    }
+
+    @GetMapping("/get/{blogId}")
+    public ApiResponse<BlogDTO> getBlog(@PathVariable int blogId) {
 
         return ApiResponse.<BlogDTO>builder()
 
-                .message("Blog created")
+                .message("Blog")
 
-                .data(blogService.createBlog(title, content, categoryId, images))
+                .data(blogService.getBlogById(blogId))
 
                 .build();
 
     }
-
-
-
-
 
 }
