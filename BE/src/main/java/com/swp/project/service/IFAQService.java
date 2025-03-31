98a5849163c3fa0430ffe6506e3b7beb4a1f396c
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:03:09 +0700

    add getAll FAQ func

--- BE/src/main/java/com/swp/project/service/IFAQService.java
+++ BE/src/main/java/com/swp/project/service/IFAQService.java
@@ -1,9 +1,13 @@
 package com.swp.project.service;
 
-
+import com.swp.project.dto.response.FAQResponse;
+import java.util.List;
 
 
 public interface IFAQService {
+
+    List<FAQResponse> getAllFAQ();
+
 
 
 
--- BE/src/main/java/com/swp/project/service/Impl/FAQService.java
+++ BE/src/main/java/com/swp/project/service/Impl/FAQService.java
@@ -1,21 +1,42 @@
 package com.swp.project.service.Impl;
 
 
+import com.swp.project.dto.response.FAQResponse;
+import com.swp.project.mapper.FAQMapper;
+import com.swp.project.repository.CategoryRepository;
+import com.swp.project.repository.FAQRepository;
 import com.swp.project.service.IFAQService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-
-
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 @RequiredArgsConstructor
 public class FAQService implements IFAQService {
 
+    private final FAQRepository faqRepository;
+    private final CategoryRepository categoryRepository;
+    private final FAQMapper faqMapper;
 
 
 
+
+    @Override
+    public List<FAQResponse> getAllFAQ() {
+
+        return faqRepository.findAllByIsDeletedFalse()
+
+                .stream()
+
+                .map(faqMapper::toDto)
+
+                .collect(Collectors.toList());
+
+
+    }
 
 
 
