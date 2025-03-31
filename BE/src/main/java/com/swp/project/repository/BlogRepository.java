f688571f21fc9768e9d0ef8d24a5f3b21631e3bb
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:35:37 +0700

    add repository

---
+++ BE/src/main/java/com/swp/project/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package com.swp.project.repository;
+
+import com.swp.project.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+}
