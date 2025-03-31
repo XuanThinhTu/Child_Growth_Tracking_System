41039c1a2738077e85964a38c17116964e5b37fd
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:32:16 +0700

    assign for doctor

--- BE/src/main/java/com/swp/project/controller/AdminController.java
+++ BE/src/main/java/com/swp/project/controller/AdminController.java
@@ -2,10 +2,12 @@ package com.swp.project.controller;
 
 import com.swp.project.dto.response.ApiResponse;
 import com.swp.project.dto.response.ChildrenDTO;
+import com.swp.project.dto.response.ConsultationRequestDTO;
 import com.swp.project.service.IChildrenService;
 import com.swp.project.service.IConsultationRequestService;
 import com.swp.project.service.IFeedbackService;
 import com.swp.project.service.IWorkingScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -33,5 +35,27 @@ public class AdminController {
                 .data(childrenDTOList)
                 .build();
     }
+
+    @Operation(summary = "This API used for ADMIN assign a doctor to a consultation request has status PENDING")
+
+    @PostMapping("/consultation/assign")
+
+    @SecurityRequirement(name = "bearerAuth")
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+
+    public ApiResponse<ConsultationRequestDTO> assignForDoctor(@RequestParam int consultationRequestId, @RequestParam int doctorId){
+        ConsultationRequestDTO consultationRequestDTO = consultationRequestService.assignDoctor(consultationRequestId, doctorId);
+
+        return ApiResponse.<ConsultationRequestDTO>builder()
+
+                .message("Consultation request assigned to doctor")
+
+                .data(consultationRequestDTO)
+
+                .build();
+
+    }
+
 
 }
