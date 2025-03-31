dcde9b81398413305f135562ba32c2e8cdccd790
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 02:24:23 +0700

    getAll feedBack func

--- BE/src/main/java/com/swp/project/service/IFeedbackService.java
+++ BE/src/main/java/com/swp/project/service/IFeedbackService.java
@@ -3,11 +3,12 @@ package com.swp.project.service;
 
 import com.swp.project.dto.request.FeedbackRequest;
 import com.swp.project.dto.response.FeedbackDTO;
+import org.springframework.data.domain.Page;
 
 public interface IFeedbackService {
 
     FeedbackDTO createFeedback(FeedbackRequest request);
-
+    Page<FeedbackDTO> getAllFeedbacks(int page, int size);
 
 
 
--- BE/src/main/java/com/swp/project/service/Impl/FeedbackService.java
+++ BE/src/main/java/com/swp/project/service/Impl/FeedbackService.java
@@ -10,6 +10,8 @@ import com.swp.project.repository.FeedbackRepository;
 import com.swp.project.repository.UserRepository;
 import com.swp.project.service.IFeedbackService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -28,32 +30,30 @@ public class FeedbackService implements IFeedbackService {
     @Override
     public FeedbackDTO createFeedback(FeedbackRequest request) {
         Feedback feedback = new Feedback();
-
-
         feedback.setRating(request.getRating());
-
         feedback.setCreatedAt(LocalDateTime.now());
-
         feedback.setComment(request.getComment());
-
         feedback.setFeedbackType(request.getFeedbackType());
-
         feedback.setMember(userService.getAuthenticatedUser());
-
         User doctor = userRepository.findById(request.getDoctorId())
-
                 .orElseThrow(() -> new RuntimeException("Doctor not found"));
-
         feedback.setDoctor(doctor);
-
-
         feedbackRepository.save(feedback);
-
-
         return feedbackMapper.toFeedbackDTO(feedback);
+    }
 
+    @Override
+    public Page<FeedbackDTO> getAllFeedbacks(int page, int size) {
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        Page<Feedback> feedbacks = feedbackRepository.findAll(pageRequest);
+
+        return feedbacks.map(feedbackMapper::toFeedbackDTO);
 
     }
+
+
 
 
 
