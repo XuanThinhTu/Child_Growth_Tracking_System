d14bfbd18f6240ee168d196f360a2663d5bacc88
TruongPhat04 <phatdtse182237@fpt.edu.vn>
2025-04-01 01:14:19 +0700

    get mapping

--- BE/src/main/java/com/swp/project/controller/FAQController.java
+++ BE/src/main/java/com/swp/project/controller/FAQController.java
@@ -1,12 +1,43 @@
 package com.swp.project.controller;
 
+import com.swp.project.dto.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.swp.project.service.Impl.FAQService;
 
 @CrossOrigin("*")
 @RequestMapping("/faq")
 @RestController
 @RequiredArgsConstructor
 public class FAQController {
+
+
+
+
+
+
+
+
+
+
+    private final FAQService faqService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<?>> getAllFAQs() {
+
+        return ResponseEntity.ok(
+
+                ApiResponse.builder()
+
+                        .message("List of FAQs")
+
+                        .data(faqService.getAllFAQ())
+
+                        .build()
+
+        );
+
+    }
 
 }
